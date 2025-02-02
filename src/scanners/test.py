b8432ee9db8e084b1bf55564a275fcc30b8aaac0
Ben West <ben.west.500@gmail.com>
2025-02-02 12:16:09 +0000

    comments

--- src/scanners/test.py
+++ src/scanners/test.py
@@ -3,6 +3,15 @@ from pathlib import Path
 
 
 def get_org_node_pkgs(repo: Path) -> dict[str, str]:
+    """
+    Opens a repos root level `package.json` and reads out any deps that start with `@types/`
+
+    Args:
+        repo: The repo to work on.
+
+    Returns:
+        Dictionary of { package : version } strings.
+    """
     pkg_json = json.load(open(repo.joinpath('package.json'), 'r'))
     deps = pkg_json['dependencies']
     deps.update(pkg_json['devDependencies'])
